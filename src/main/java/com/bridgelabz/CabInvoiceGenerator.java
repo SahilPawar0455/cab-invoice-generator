ca65f385ace8427cf9be1cf0a59b8bc07238227b
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-27 19:24:41 +0530

    ADDED: given distance and time the invoice genertor should return the total fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,4 +1,11 @@
 package com.bridgelabz;
 
 public class CabInvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+    public static double calculateTotalFare(double distance, double time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
 }
--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -1,4 +1,17 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CabInvoiceTest {
+    @Test
+    public void givenDistanceAndTimeShouldReturnTotalFare() {
+        double totalFare = CabInvoiceGenerator.calculateTotalFare(2.0, 4.0);
+        Assert.assertEquals(24.0, totalFare, 0.0);
+    }
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+        double fare = CabInvoiceGenerator.calculateTotalFare(0.1, 1.0);
+        Assert.assertEquals(5, fare, 0.0);
+    }
 }
