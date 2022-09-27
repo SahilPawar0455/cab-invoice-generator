e1ac1b0938336f0232540d6f4466bcc949dfd343
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-27 19:32:55 +0530

    ADDED:Given the multiply rides and calculate the aggregate total for all

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.List;
+
 public class CabInvoiceGenerator {
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
@@ -7,5 +9,13 @@ public class CabInvoiceGenerator {
     public static double calculateTotalFare(double distance, double time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    public static double calculateTotalFare(List<Ride> rideList) {
+        double aggregate = 0;
+        for (Ride ride : rideList) {
+            aggregate += calculateTotalFare(ride.distance, ride.time);
+        }
+        return aggregate;
     }
 }
--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CabInvoiceTest {
     @Test
     public void givenDistanceAndTimeShouldReturnTotalFare() {
@@ -13,5 +16,19 @@ public class CabInvoiceTest {
     public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
         double fare = CabInvoiceGenerator.calculateTotalFare(0.1, 1.0);
         Assert.assertEquals(5, fare, 0.0);
+    }
+    @Test
+    public void givenMultipleRidesShouldReturnAggregateFare() {
+        Ride ride1 = new Ride(5, 10);
+        Ride ride2 = new Ride(4, 8);
+        Ride ride3 = new Ride(2, 9);
+        Ride ride4 = new Ride(6, 3);
+        List<Ride> rideList = new ArrayList<>();
+        rideList.add(ride1);
+        rideList.add(ride2);
+        rideList.add(ride3);
+        rideList.add(ride4);
+        double totalFare = CabInvoiceGenerator.calculateTotalFare(rideList);
+        Assert.assertEquals(200.0, totalFare, 0.0);
     }
 }
